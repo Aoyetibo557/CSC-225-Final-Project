ee91adbb10a6d4276b8d97e55a64c71d36a8a8b8
Anuoluwapo Oyetibo <Aoyetibo@gmail.com>
2019-12-19 17:11:48 -0500

    csc 225 wqFinal Project

---
+++ script.js
@@ -0,0 +1,50 @@
+document.body.className ="loading";
+
+jQuery(document).ready(function($){
+
+    axios.get('http://csc225.mockable.io/movies').then(function(response){
+
+        console.log(response.data);
+        var moviesHtml = response.data.map(function(movie){
+            //return $('<p>').addClass('movie').data('movie',movie.data).html(movie.title)
+            //or
+            return '<p class="movie" data-movie="' + movie.id +'">'+movie.title+'</p>'
+        });
+        
+        //printing to console and document
+        console.log(moviesHtml);
+        $('#Mymovies').html(moviesHtml);
+        document.body.className=""; 
+
+
+    });
+
+
+
+    //getting the movie url and id attached when movie is clicked on
+    $('body').on('click','.movie',function(){
+
+        var id  =$(this).data('movie');
+        var url = 'https://csc225.mockable.io/movies/' + id;
+
+
+        $('#current-movie').html('<img src="/loading3.gif">');
+        axios.get(url).then(function(response){
+            var movie=response.data;  
+            var movieHtml = ' <p> Title: '+movie.title+'</p>';
+            movieHtml +='<p> Director: ' + movie.director + '</p>';
+            movieHtml +='<p> IdNo: '+movie.id+'</p>';
+            movieHtml +='<p> Release Yr: '+movie.release+'</p>';
+            movieHtml +='<a href="'+movie.poster +'">[POSTER-LINK]</a>';
+            
+            $('#current-movie').html(movieHtml);
+            console.log(movieHtml);
+
+
+        });
+       
+
+        
+    });
+
+});
